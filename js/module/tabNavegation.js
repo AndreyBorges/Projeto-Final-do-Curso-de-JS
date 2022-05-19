8eb3fcce23f227d3c629fb7b61eb67478e63b4e3
Andrey  Airson <andrey@vallorisolutions.com>
2022-05-19 20:08:16 -0300

    refact: TabNav Function for Class

--- js/module/tabNavegation.js
+++ js/module/tabNavegation.js
@@ -1,21 +1,30 @@
-export default function initTabNavegation() {
-  const tab = document.querySelectorAll("[data-tab='menu'] li");
-  const tabText = document.querySelectorAll("[data-tab='content'] section");
-  tabText[0].classList.add("active");
+export default class TabNavegation {
+  constructor(list, content) {
+    this.tab = document.querySelectorAll(list);
+    this.tabText = document.querySelectorAll(content);
+    this.active = "active";
+    // tabText[0].classList.add(this.active);
+  }
 
-  if (tab.length && tabText.length) {
-    const activeTab = (idx) => {
-      const direction = tabText[idx].dataset.anime;
-      tabText.forEach((item) => {
-        item.classList.remove("active", direction);
-      });
-      tabText[idx].classList.add("active", direction);
-    };
-
-    tab.forEach((item, idx) => {
-      item.addEventListener("click", () => {
-        activeTab(idx);
-      });
+  activeTab(idx) {
+    const direction = this.tabText[idx].dataset.anime;
+    this.tabText.forEach((item) => {
+      item.classList.remove(this.active, direction);
     });
+    this.tabText[idx].classList.add(this.active, direction);
+  }
+
+  // adiciona o evento de click no elemento
+
+  addEventTab() {
+    this.tab.forEach((item, idx) => {
+      item.addEventListener("click", () => this.activeTab(idx));
+    });
+  }
+
+  init() {
+    this.tab && this.tabText
+      ? (this.addEventTab(),this.activeTab(0))
+      : null;
   }
 }
