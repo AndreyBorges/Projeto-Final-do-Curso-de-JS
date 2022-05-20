001ff26e03c4e19f20ed50182d57d16af5705893
Andrey  Airson <andrey@vallorisolutions.com>
2022-05-19 23:14:29 -0300

    refact: Fetch Animais Function for Class

--- js/module/fetchAnimais.js
+++ js/module/fetchAnimais.js
@@ -1,21 +1,7 @@
 import Numbers from "./numbers.js";
 
-export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
-    try {
-      const animaisResp = await fetch(url);
-      const animais = await animaisResp.json();
-      const gridNumbers = document.querySelector(".gridNumbers");
-      animais.forEach((animal) => {
-        const animalSpecies = createElement(animal.specie, animal.total);
-        gridNumbers.appendChild(animalSpecies);
-      });
-      const numbers = new Numbers('[data-number]','.numbers', 'active');
-      numbers.init();
-    } catch (error) {
-      console.log(error);
-    }
-  }
+export default function initFetchAnimais(url, target) {
+  // cria a div que terá o total de animais
 
   const createElement = (name, numbers) => {
     const div = document.createElement("div");
@@ -30,5 +16,31 @@ export default function initFetchAnimais() {
     return div;
   };
 
-  fetchAnimais("./dataAnimais.json");
+  // anima os numeros dos animais
+
+  const animaNumbers = () => {
+    const numbers = new Numbers("[data-number]", ".numbers", "active");
+    numbers.init();
+  };
+
+  // puxa as informações do json e cria um objeto com as informações adiquiridas do json
+
+  async function fetchAnimais() {
+    try {
+      // faz o fetch do json espera a resposta e depois transforma em json para tratar os dados.
+
+      const animaisResp = await fetch(url);
+      const animais = await animaisResp.json();
+      const gridNumbers = document.querySelector(target);
+      animais.forEach((animal) => {
+        const animalSpecies = createElement(animal.specie, animal.total);
+        gridNumbers.appendChild(animalSpecies);
+      });
+      animaNumbers();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return fetchAnimais();
 }
