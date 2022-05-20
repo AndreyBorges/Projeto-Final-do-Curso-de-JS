4477b3e259e70a30c0df6466dc58958f6cff7e29
Andrey  Airson <andrey@vallorisolutions.com>
2022-05-20 15:05:50 -0300

    refact: Menu Mobile Function for Class

--- js/module/menuMobile.js
+++ js/module/menuMobile.js
@@ -1,18 +1,41 @@
 import OutSideClick from "./outSide.js";
 
-export default function initMenuMobile() {
-  const menuBtn = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const events = ["click", "touchstart"];
-  if (menuBtn) {
-    function openMenu() {
-      menuList.classList.add("active");
-      menuBtn.classList.add("active");
-      OutSideClick(menuList, events, () => {
-        menuList.classList.remove("active");
-        menuBtn.classList.remove("active");
-      });
-    }
-    events.forEach((ev) => menuBtn.addEventListener(ev, openMenu));
+export default class MenuMobile {
+  constructor(menuBtn, menuList, evts) {
+    this.menuBtn = document.querySelector(menuBtn);
+    this.menuList = document.querySelector(menuList);
+    this.active = "active";
+
+
+    // bind pois tem um evento de callback 
+
+    this.openMenu = this.openMenu.bind(this);
+
+
+    // define como touchstart ou click como padrão dos events caso não seja passado nenhum
+
+    evts === undefined
+      ? (this.events = ["touchstart", "click"])
+      : (this.events = evts);
+  }
+
+  openMenu() {
+    this.menuList.classList.add(this.active);
+    this.menuBtn.classList.add(this.active);
+    OutSideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.active);
+      this.menuBtn.classList.remove(this.active);
+    });
+  }
+
+  menuEvents() {
+    this.events.forEach((ev) =>
+      this.menuBtn.addEventListener(ev, this.openMenu)
+    );
+  }
+
+  init() {
+    this.menuBtn && this.menuList ? this.menuEvents() : null;
+    return this;
   }
 }
