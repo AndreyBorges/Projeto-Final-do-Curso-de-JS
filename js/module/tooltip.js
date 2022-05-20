a5538844dced968320d60b4f7a575eb8892a858a
Andrey  Airson <andrey@vallorisolutions.com>
2022-05-19 21:43:22 -0300

    refact: Tooltip Function for Class

--- js/module/tooltip.js
+++ js/module/tooltip.js
@@ -1,43 +1,63 @@
-export default function initTooltip() {
-  const tooltips = document.querySelectorAll("[data-tooltip]");
-  const onMouseLeaveTooltip = {
-    tooltipBox: "",
-    element: "",
-    handleEvent() {
-      this.tooltipBox.remove();
-      this.element.removeEventListener("mouseleave", onMouseLeaveTooltip);
-      this.element.removeEventListener("mousemove", onMouseMove);
-    },
-  };
-  const onMouseMove = {
-    tooltipBox: "",
-    element: "",
-    handleEvent({ pageY, pageX }) {
-      this.tooltipBox.style.top = `${pageY + 10}px`;
-      this.tooltipBox.style.left = `${pageX + 10}px`;
-    },
-  };
+import initAnimationScroll from "./animationScroll";
 
-  const createTooltipBox = (el) => {
+export default class Tooltip {
+  constructor(tooltips) {
+    this.tooltips = document.querySelectorAll(tooltips);
+
+    // bind do obj da classe aos callbacks
+
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  // move a tooltip com base no mouse
+
+  onMouseMove({ pageY, pageX }) {
+    this.tooltipBox.style.top = `${pageY + 10}px`;
+    pageX > window.innerWidth / 2
+      ? (this.tooltipBox.style.left = `${pageX - 190}px`)
+      : (this.tooltipBox.style.left = `${pageX + 10}px`);
+  }
+
+  // remove a tooltipBox do DOM quando o mouse sai do elemento
+
+  onMouseLeave({ currentTarget }) {
+    this.tooltipBox.remove();
+    currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
+    currentTarget.removeEventListener("mousemove", this.onMouseMove);
+  }
+
+  // cria a tooltipBox e adiciona ao body
+
+  createTooltipBox = (el) => {
     const tooltipBox = document.createElement("div");
     const textTooltip = el.getAttribute("aria-label");
     tooltipBox.classList.add("tooltip");
     tooltipBox.innerText = textTooltip;
     document.body.appendChild(tooltipBox);
-    return tooltipBox;
+    this.tooltipBox = tooltipBox;
   };
 
-  tooltips.forEach((item) => {
-    function mouseOverTooltip({ target }) {
-      const tooltipBox = createTooltipBox(target);
+  // cria a tooltipBox e adiciona os eventos de mousemove e mouseleave 
 
-      onMouseMove.tooltipBox = tooltipBox;
-      target.addEventListener("mousemove", onMouseMove);
+  onMouseOver({ currentTarget }) {
+    // cria a tooltipBox e coloca uma propriedade
 
-      onMouseLeaveTooltip.tooltipBox = tooltipBox;
-      onMouseLeaveTooltip.element = target;
-      target.addEventListener("mouseleave", onMouseLeaveTooltip);
-    }
-    item.onmouseover = mouseOverTooltip;
-  });
+    this.createTooltipBox(currentTarget);
+    currentTarget.addEventListener("mousemove", this.onMouseMove);
+    currentTarget.addEventListener("mouseleave", this.onMouseLeave);
+  }
+
+  // adiciona os eventos de mouseOver em cada tooltip do seu site
+
+  tooltipsEvent() {
+    this.tooltips.forEach((item) =>
+      item.addEventListener("mouseover", this.onMouseOver)
+    );
+  }
+  init() {
+    this.tooltips.length ? this.tooltipsEvent() : null;
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import Scroll from "./module/scrollSuave.js";
 import Accordion from "./module/accordion.js";
 import TabNavegation from "./module/tabNavegation.js";
 import Modal from "./module/modal.js";
-import initTooltip from "./module/tooltip.js";
+import Tooltip from "./module/tooltip.js";
 import initDropdown from "./module/dropDown.js";
 import initMenuMobile from "./module/menuMobile.js";
 import initFuncionamento from "./module/funcionamento.js";
@@ -14,12 +14,13 @@ const scrollSuave = new Scroll('[data-menu="suave"] a[href^="#"]');
 const accordionList = new Accordion("[data-anime='accordion'] dt");
 const tabNav = new TabNavegation("[data-tab='menu'] li","[data-tab='content'] section")
 const modalLogin = new Modal ("[data-modal='open']", "[data-modal='close']", ".modalContainer");
+const tooltip = new Tooltip("[data-tooltip]");
 
 scrollSuave.init();
 accordionList.init();
 tabNav.init();
 modalLogin.init()
-initTooltip();
+tooltip.init();
 initDropdown();
 initMenuMobile();
 initFuncionamento();
